ca02852cbb55a852084df0199a6099f2e96fb835
andrewskyboss <andrew.cybossky@gmail.com>
2021-05-01 14:41:02 +0000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
 $('.carousel').carousel({
   interval: 2000
 })
+
+/*----------- Mobile menu Open Close ----------*/
+$('body').on('click', '.mobile-menu-trigger', function(e) {
+	if(!$(this).hasClass('.mobile-menu-visible')) {
+		mobileMenuOpenClose(true);
+	} else {
+		mobileMenuOpenClose(false);
+	}
+	e.stopPropagation();
+})
+.on('click', function(e) {
+	mobileMenuOpenClose(false);
+});
+
+function mobileMenuOpenClose(open) {
+	open = (typeof open !== 'undefined') ? open : true;
+
+	if(open) {
+		$('.mobile-menu').show().removeClass('hidden').addClass('visible');
+		$('.mobile-menu-trigger').removeClass('mobile-menu-hidden').addClass('mobile-menu-visible');
+		/*$('body').css('overflow', 'hidden');*/
+	} else {
+		$('.mobile-menu').removeClass('visible').addClass('hidden');
+		$('.mobile-menu-trigger').removeClass('mobile-menu-visible').addClass('mobile-menu-hidden');
+		/*$('body').css('overflow', 'auto');*/
+	}
+}
+
