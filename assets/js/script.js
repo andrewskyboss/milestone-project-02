f6cd46192e52f5a1cf7ef503bebbe6f7c4af0641
andrewskyboss <andrew.cybossky@gmail.com>
2021-05-04 21:12:50 +0000

    about us

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 /*----------- Carousel Bootstrap ----------*/
 $('.carousel').carousel({
   interval: 2000
@@ -20,7 +21,7 @@ function mobileMenuOpenClose(open) {
 	open = (typeof open !== 'undefined') ? open : true;
 
 	if(open) {
-		$('.mobile-menu').show().removeClass('hidden').addClass('visible');
+		$('.mobile-menu').show('slow').removeClass('hidden').addClass('visible');
 		$('.mobile-menu-trigger').removeClass('mobile-menu-hidden').addClass('mobile-menu-visible');
 	} else {
 		$('.mobile-menu').removeClass('visible').addClass('hidden');
@@ -30,16 +31,38 @@ function mobileMenuOpenClose(open) {
 /*----------- Newsletter Sign up ----------*/
 function newsletterMail(newsletterForm) {
 	emailjs.send("service_u9b5bpg","template_dm2c1ma", {
-	"from_email": newsletterForm.emailNewsletters.value
+		"from_email": newsletterForm.emailNewsletters.value
 	})
 	.then (
 		function(response) {
 			console.log("SUCCESS", response);
 		},
 		function(error) {
-			console.log("FAILED", error);
+			console.log("FAILED . . .", error);
 		}
 	);
-	return false;
+	return true;
 }
+/*----------- Contact Form ----------*/
 
+window.onload = function() {
+	document.getElementById('contact-us-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+		emailjs.sendForm('service_u9b5bpg', 'template_a26hqai', this)
+		.then(function() {
+			console.log('SUCCESS!');
+			this.firstName.value = "";
+			this.lastName.value = "";
+			this.emailAddress.value = "";
+			this.phoneNumber.value = "";
+			this.address.value = "";
+			this.city.value = "";
+			this.country.value = "";
+			this.message.value = "";
+
+		}, function(error) {
+			console.log('FAILED...', error);
+		});
+	});
+}
